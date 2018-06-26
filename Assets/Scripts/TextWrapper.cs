dcabd825e29bc4a5eee64179a799b3b2cc674e8e
nek27 <12-11469@usb.ve>
2018-06-25 20:04:32 -0400

    Initial commit
    Initial commit with testing files

---
+++ Assets/Scripts/TextWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class TextWrapper{
+	// Wrap text by line length and total lines
+	public static string WrappText(string input, int lineLength, int n_lines){
+		
+		string[] words = input.Split(' ');
+		string result = "";
+		string line = "";
+		int count = 1;
+		
+		foreach(string w in words){
+			string temp = line + " " + w;
+			
+			if(temp.Length > lineLength){
+				result += line + "\n";
+				line = w;
+				count += 1;
+			}else{
+				line = temp;
+			}
+			if(count >= n_lines-1){
+				break;
+			}
+		}
+		result += line;
+		
+		return result.Substring(1, result.Length-1);
+	}
+}
